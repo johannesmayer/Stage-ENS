125657640e3640787d4d8226db4d10ad3ca54af6
johannesmayer <j.mayer@stud.uni-heidelberg.de>
2015-01-15 11:43:00 +0100

    Improved program
    uses sys.argv as input plus bug in magn core gone

---
+++ ApplicationFactorized/Ising/Simulations/DataAnalysis/autocorrelation.py
@@ -0,0 +1,93 @@
+import numpy, sys
+
+
+def autocorrelation(my_list, delta):
+    mean = numpy.mean(my_list[delta:]*my_list[:-delta])
+    return mean
+   
+
+is_cluster = sys.argv[1]
+stand_title = sys.argv[2]   
+fact_title = sys.argv[3]    
+     
+# give the standard metropolis data to data1 and the factorized one to data2   
+
+if is_cluster == "True":
+    is_cluster = True
+if is_cluster == "False":
+    is_cluster = False
+    
+#stand_title = "local_stand_beta_crit.npy"
+#fact_title = "local_fact_beta_crit.npy"
+
+data1 = numpy.load("SimulationData/"+stand_title)
+data2 = numpy.load("SimulationData/"+fact_title)
+
+
+
+e_data1 = data1[0]
+e_data2 = data2[0]
+
+m_data1 = data1[1]
+m_data2 = data2[1]
+
+
+
+axis = []
+
+e_correlation_function_stand = []
+e_correlation_function_fact = []
+
+m_correlation_function_stand = []
+m_correlation_function_fact = []
+
+e_mean_stand = numpy.mean(e_data1)
+e_mean_fact = numpy.mean(e_data2)
+
+e_variance_stand = numpy.mean(e_data1*e_data1)
+e_variance_fact = numpy.mean(e_data2*e_data2)
+
+
+
+
+m_correlation_function_stand = []
+m_correlation_function_fact = []
+
+m_mean_stand = numpy.mean(m_data1)
+m_mean_fact = numpy.mean(m_data2)
+
+m_variance_stand = numpy.mean(m_data1*m_data1)
+m_variance_fact = numpy.mean(m_data2*m_data2)
+
+
+correlation_max = 1000
+stepsize = 1
+if is_cluster == False:
+    stepsize = 10
+    correlation_max = 10000
+
+for index in range(1,correlation_max,stepsize):
+    if index % 11 == 0:
+        print("Progress: "+str(index)+"/"+str(correlation_max))
+        
+    e_correlation_function_stand.append((autocorrelation(e_data1,index)-e_mean_stand**2 )/ (e_variance_stand - e_mean_stand**2))
+    e_correlation_function_fact.append((autocorrelation(e_data2,index)-e_mean_fact**2 )/ (e_variance_fact - e_mean_fact**2))
+   
+    m_correlation_function_stand.append((autocorrelation(m_data1,index)-m_mean_stand**2 )/ (m_variance_stand - m_mean_stand**2))
+    m_correlation_function_fact.append((autocorrelation(m_data2,index)-m_mean_fact**2 )/ (m_variance_fact - m_mean_fact**2))
+    
+    axis.append(index)
+ 
+
+correlation_stand = [axis, e_correlation_function_stand, m_correlation_function_stand]
+correlation_fact = [axis, e_correlation_function_fact, m_correlation_function_fact]
+
+numpy.save("AnalysisData/correlation_"+stand_title,correlation_stand)
+numpy.save("AnalysisData/correlation_"+fact_title,correlation_fact)
+    
+    
+    
+
+
+
+
---
+++ ApplicationFactorized/Ising/Simulations/local_metrop.py
@@ -0,0 +1,105 @@
+import numpy, random, math, time, sys
+
+start_time = time.time()
+
+def square_neighbors(L):
+    N = L*L
+    site_dic = {}
+    x_y_dic = {}
+    for j in range(N):
+        row = j//L
+        column = j-row*L
+        site_dic[(row,column)] = j
+        x_y_dic[j] = (row,column)
+    nbr = []
+    for j in range(N):
+        row,column = x_y_dic[j]
+        right_nbr = site_dic[(row,(column+1)%L)]
+        left_nbr = site_dic[(row,(column-1+L)%L)]
+        up_nbr = site_dic[((row-1+L)%L,column)]
+        down_nbr = site_dic[((row+1)%L,column)]
+        nbr.append((right_nbr,left_nbr,up_nbr,down_nbr))
+    nbr = tuple(nbr)
+    return nbr,site_dic,x_y_dic
+    
+def energy(S,nbr):
+    ene = 0.0
+    for index in range(len(S)):
+        h = sum(S[nbr[index][j]] for j in range(4))
+        ene += -h*S[index]/2
+    return ene
+    
+def magnetisation(S):
+    magn = numpy.mean(S)
+    return magn
+        
+factorized = False
+        
+N_iter = 2**20
+
+
+L = 6
+N = L*L
+
+beta = float(sys.argv[1])
+#beta = math.log(1+math.sqrt(2))/2
+#beta = 0.1
+
+nbr, site_dic, x_y_dic = square_neighbors(L)
+S=[random.choice([-1,1]) for k in range(N)]
+
+
+accept_index = 0.0
+
+internal_energy = energy(S,nbr)
+
+energies = []
+magnetisations = []
+
+
+for i_sweep in range(N_iter):
+    if i_sweep % 1000 == 0:
+        print("Progress: "+str(i_sweep)+"/"+str(N_iter))
+    #here pick a random spin and look at the surroundings
+    k=random.randint(0,N-1)
+    h = sum(S[nbr[k][j]] for j in range(4))
+    upsilon = 0.0
+    if factorized == True:
+        del_ener = []
+    #look what energy difference a flip of the chosen spin would make with each 
+    #of its neighbours
+        for j in range(4):
+            del_ener.append(2*S[nbr[k][j]]*S[k])
+    #apply the factorized metropolis p_acc(a->b) = prod(min(1,exp(-beta*delta(E_ij)))
+        bracket_sum = 0.0
+        for iter in range(4):
+            bracket_sum += max(0,del_ener[iter])        
+        upsilon = math.exp(-beta*bracket_sum)
+    else:
+        upsilon = min(1,math.exp(-beta*2*S[k]*h))
+    if random.uniform(0.,1.) < upsilon:
+        S[k] = -S[k]
+        # delta E = 2 * oldspin * h so since we flipped we have to subtract 2hS[k]
+        internal_energy = internal_energy - 2*h*S[k]
+        accept_index += 1
+    energies.append(internal_energy/N)
+    magnetisations.append(magnetisation([S]))
+    
+print("Internal Energy pp: " + str(numpy.mean(energies)))
+print("Magnetisation pp: " + str(numpy.mean(magnetisations)))
+print("acceptance ratio: " + str(accept_index/N_iter))       
+            
+    
+print("Duration: "+str(time.time() - start_time))    
+
+local = [energies, magnetisations]
+
+if factorized == True:
+    numpy.save("DataAnalysis/SimulationData/local_fact_beta_"+str(beta)[:6]+".npy",local)
+    
+    
+if factorized == False:
+    numpy.save("DataAnalysis/SimulationData/local_stand_beta_"+str(beta)[:6]+".npy",local)
+   
+
+    
