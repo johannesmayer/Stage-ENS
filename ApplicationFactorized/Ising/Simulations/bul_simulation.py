e99696c56a00f5fef381d7d39a65cd208c8b4857
johannesmayer <j.mayer@stud.uni-heidelberg.de>
2015-01-16 11:27:21 +0100

    BUL simulation

--- ApplicationFactorized/Ising/Simulations/bul_simulation.py
+++ ApplicationFactorized/Ising/Simulations/bul_simulation.py
@@ -49,22 +49,67 @@ def name_class(h,s):
     return int(which_class)
 
 def compute_classes(S,nbr):
-    classes = numpy.zeros(10)
+    classes = [[] for k in range(10)]
     for j in range(len(S)):
         h = sum(S[nbr[j][k]][0] for k in range(4))
         which_class = name_class(h,S[j][0])
-        classes[which_class] += 1
+        classes[which_class].append(j)
         S[j][1] = which_class
     return classes        
  
-    
+def ffunction(number):
+    return ((number + 5 ) % 10)
+ 
+def towersample(tower):   
+    eta = random.uniform(0.,max(tower))
+    saturday_activity = 0
+    for index in range(len(tower)):
+        if eta < tower[index]:
+            saturday_activity = index
+            break
+    return saturday_activity
+
+def built_tower(probabilities):
+    tow = []
+    cumulative = 0.0
+    for prob in probabilities:
+        cumulative += prob
+        tow.append(cumulative)     
+    return tow
+
+def built_probs(N,beta,is_fact):
+    p = numpy.zeros(10)
+    if is_fact == True:
+        for k in range(10):
+            if k < 5:
+                p[k] = (1/float(N)) * (min(1,math.exp(-2*beta))**(4-k))
+            else:
+                p[k] = p[9-k] 
+           
+    if is_fact == False:                        
+        for k in range(10):
+            if k < 5:
+                p[k] = (1/float(N)) * (min(1,math.exp(-2*beta*(2-k))))
+            else:
+                p[k] = p[9-k]
+    return p 
+                                                                                   
 L = 6
 N = L*L
+beta = 0.1
+
+N_iter = 2**8
+
+
 
 nbr, site_ic, x_y_dic = square_neighbors(L)
 S = [[random.choice([-1,1]),0] for j in range(N)]
 
 class_members = compute_classes(S,nbr)
+probs = built_probs(N,beta,False)
+tower = built_tower(probs)
+
+
 
 
 
